3ec3d73b3b21b890c19bff18102cd3b5d394fa7f
Joshua Goldie <joshua.goldie@utc.com>
2019-04-22 16:48:03 -0400

    Initial untested version of Kotlin typed persistent actor.

---
+++ src/main/kotlin/com/utc/es/TestTypedPersistence.kt
@@ -0,0 +1,48 @@
+package com.utc.es
+
+
+import akka.persistence.typed.PersistenceId
+import akka.persistence.typed.javadsl.CommandHandler
+import akka.persistence.typed.javadsl.EventHandler
+import akka.persistence.typed.javadsl.EventSourcedBehavior
+
+object TestTypedPersistence {
+
+    sealed class Command {
+        data class Add(val item: String) : Command()
+        class Clear : Command()
+    }
+
+    sealed class Event {
+        data class Added(val item: String) : Event()
+        class Cleared : Event()
+    }
+
+    data class State(val items: List<String> = emptyList()) {
+        fun addItem(item: String): State = State((listOf(item) + items).take(5))
+    }
+
+    class TestTypedPersistence(persistenceId: PersistenceId) : EventSourcedBehavior<Command, Event, State>(persistenceId) {
+
+        override fun emptyState(): State = State()
+
+        override fun commandHandler(): CommandHandler<Command, Event, State> =
+                CommandHandler<Command, Event, State> { state: State, command: Command ->
+                    when (command) {
+                        is Command.Add -> Effect().persist(Event.Added(command.item))
+                        is Command.Clear -> Effect().persist(Event.Cleared())
+                    }
+                }
+
+
+        override fun eventHandler(): EventHandler<State, Event> =
+                EventHandler<State, Event> { state, event ->
+                    when (event) {
+                        is Event.Added -> state.addItem(event.item)
+                        is Event.Cleared -> emptyState()
+                    }
+                }
+
+    }
+
+}
